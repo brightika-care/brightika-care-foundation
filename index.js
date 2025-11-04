fef0fa4f66364b51737a4d10515168f89cd6c1c8
brightika-care <wolverine.logan741@gmail.com>
2025-11-04 09:20:55 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      fontFamily: "Arial",
+    }}>
+      <h1>Welcome to Brightika Care Foundation</h1>
+      <p>Your site is live and working on Vercel 🎉</p>
+    </div>
+  );
+}
